f6b7244a91e52f7b5b44ca716bb36b0f61e2160d
cdabo <cmdabo@gmail.com>
2019-08-18 11:25:46 +1000

    no message

---
+++ ProjectLima/Assets/Dasbor/Scripts/FishBrain.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishBrain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 direction;
+    //Rigidbody2D rb2d;
+
+    public float speed = 2f;
+    void Start()
+    {
+        //rb2d = transform.parent.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.parent.position = Vector2.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        this.direction = direction;
+    }
+}
---
+++ ProjectLima/FishSpawnerBrain.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawnerBrain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float minTime = 1;
+    public float maxTime = 4;
+
+    float spawnTime = 0;
+
+    public GameObject topLeftBoundary, topRightBoundary, bottomLeftBoundary, bottomRightBoundary;
+
+    public GameObject fishPrefab;
+
+    GameObject[,] boundaries;
+
+    void Start()
+    {
+        spawnTime = Time.deltaTime + Random.Range(minTime, maxTime);
+        boundaries = new GameObject[,]
+        {
+            { topLeftBoundary, bottomLeftBoundary },
+            {topRightBoundary, bottomRightBoundary }
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.deltaTime > spawnTime)
+        {
+            SpawnFish();
+        }
+    }
+
+    void SpawnFish()
+    {
+        int sideOfScreen = Random.Range(0, 2);
+        float transformY = Random.Range(boundaries[sideOfScreen, 0].transform.position.y, boundaries[sideOfScreen, 1].transform.position.y);
+        Debug.Log(boundaries[sideOfScreen, 0].transform.position.x);
+        GameObject newFish = (GameObject)Instantiate(fishPrefab, new Vector2(boundaries[sideOfScreen, 0].transform.position.x, transformY), transform.rotation);
+        Debug.Log("222");
+        newFish.GetComponent<FishBrain>().SetDestination(new Vector2(sideOfScreen == 1 ? topLeftBoundary.transform.position.x : topRightBoundary.transform.position.x, transformY));
+    }
+}
